d6c412ceaaa0b923b76906d1e8e7c6517283c66b
Arundhathi-Nayak <nayakarundhathi5@gmail.com>
2024-03-10 17:10:56 +0530

    Testing

--- DLTE-JAVA/DLTE-JAVA-TASKS/LoanProductEntity/src/test/java/org/example/AppTest.java
+++ DLTE-JAVA/DLTE-JAVA-TASKS/LoanProductEntity/src/test/java/org/example/AppTest.java
@@ -1,9 +1,12 @@
 package org.example;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -14,39 +17,64 @@ import static org.junit.Assert.*;
 public class AppTest 
 {
     private BankApp bankApp;
+    static List<Loan> loanList=new ArrayList<>();
+    static List<Loan> loan=new ArrayList<>();
     @Before
     public void setUp() {
         bankApp = new BankApp();
     }
-    public void testAddNewLoan() throws IOException, ClassNotFoundException, IOException {
+
+    @BeforeClass
+    public static void initialize(){
+        loanList.add(new Loan(123,1000,"2/2/2024","Aruu","open","9876543"));
+        loanList.add(new Loan(223,2000,"3/12/2024","Annapoo","open","9876543"));
+        loanList.add(new Loan(323,3000,"4/20/2024","Divija","open","9876543"));
+        loanList.add(new Loan(423,4000,"5/6/2024","Eeksha","open","9876543"));
+        loanList.add(new Loan(523,5000,"6/8/2024","Avinash","open","9876543"));
+    }
+    @Test
+    public void testAddNewLoan() throws IOException, ClassNotFoundException {
         int initialSize = bankApp.availableLoan().size();
         bankApp.addNewLoan();
         int newSize = bankApp.availableLoan().size();
         assertEquals(initialSize + 1, newSize);
     }
-    public void testAvailableLoan() throws IOException, ClassNotFoundException {
-        List<Loan> availableLoans = bankApp.availableLoan();
-        assertNotNull(availableLoans);
-    }
 
 
 
     @Test
-    public void testClosedLoan() throws IOException, ClassNotFoundException {
-        List<Loan> closedLoans = bankApp.closedLoan();
-        assertNotNull(closedLoans);
+    public void testClosedLoan()  {
+        String expectedLoanStatus="closed";
+        assertNotEquals("Expected test to pass",expectedLoanStatus,loanList.get(0).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(0).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(1).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(2).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(3).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(4).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(5).getLoanStatus());
     }
 
     @Test
-    public void testOpenLoan() throws IOException, ClassNotFoundException {
+    public void testOpenLoan() {
         // Assuming open loans have a status of "open"
-        List<Loan> openLoans = bankApp.availableLoan();
-        assertNotNull(openLoans);
-        for (Loan loan : openLoans) {
-            assertEquals("open", loan.getLoanStatus());
-        }
+        String expectedLoanStatus="open";
+        assertNotEquals("Expected test to pass",expectedLoanStatus,loanList.get(0).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(1).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(2).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(3).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(4).getLoanStatus());
+        assertEquals(expectedLoanStatus,loanList.get(5).getLoanStatus());
     }
-
+    @Test(timeout =1000)
+    public void testTime() throws InterruptedException {
+        Thread.sleep(100);
+        assertTrue(loanList.size()>0);
+    }
+    @Test
+    public void checkNull(){
+        assertNull(loan);
+        assertNotNull(loanList);
+    }
 
 
 
